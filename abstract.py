43d0b0995217fa28cac8d059a2bf44d030783d24
Selvarani CR <Selvarani.cr@datagrokr.com>
2024-04-25 11:49:04 +0530

    created an abstract class Person() and 2 cild classes

---
+++ abstract.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class Person(ABC):
+    @abstractmethod
+    def get_gender(self):
+        pass
+
+class Male(Person):
+    def get_gender(self):
+        return "Male"
+
+class Female(Person):
+    def get_gender(self):
+        return "Female"
+
+male = Male()
+female = Female()
+
+print("Male gender:", male.get_gender())
+print("Female gender:", female.get_gender())
