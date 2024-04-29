3bb04095614e1335f4a6ef1effe7b70d1bf33c55
Selvarani CR <Selvarani.cr@datagrokr.com>
2024-04-29 11:06:51 +0530

    Program to print 3*5*8 3d array

--- 3d_array.py
+++ 3d_array.py
@@ -1,17 +1,6 @@
-array = []
+import numpy as np
 
-# for i in range(3):
-#     layer = []
-#     for j in range(5):
-#         row = []
-#         for k in range(8):
-#             row.append(0)
-#         layer.append(row)
-#     array.append(layer)  
+array_3d = np.zeros((3, 5, 8))
+array_str = np.array_str(array_3d).replace('.', ',')
 
-array = [[[0 for _ in range(8)] for _ in range(5)] for _ in range(3)]
-
-for layer in array:
-    for row in layer:
-        print(row)
-    print()              
+print(array_str)
